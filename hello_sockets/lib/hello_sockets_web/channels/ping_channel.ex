c7db83f7bfa31b79ec85eab26d44c972b977aa2c
Spencer Olson <solson@instructure.com>
2024-05-25 15:02:15 -0500

    add noreply and shutdown examples for ping channel

--- hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
+++ hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
@@ -6,10 +6,22 @@ defmodule HelloSocketsWeb.PingChannel do
     {:ok, socket}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
+  @impl true
+  def handle_in("ping", %{"ack_phrase" => ack_phrase}, socket) do
+    {:reply, {:ok, %{ping: ack_phrase}}, socket}
+  end
+
   @impl true
   def handle_in("ping", _payload, socket) do
     {:reply, {:ok, %{ping: "pong"}}, socket}
+  end
+
+  # we only handle ping
+  def handle_in("pong", _payload, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_in("ding", _payload, socket) do
+    {:stop, :shutdown, {:ok, %{msg: "shutting down"}}, socket}
   end
 end
